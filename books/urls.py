0da04c419066d679dbb2b1a5afaf637c46aa7276
SANJIKS <sanzarmaratov588@gmail.com>
2024-10-16 15:07:01 +0600

    available books

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookViewSet, PageDetailView, ReceiptViewSet
+from .views import AvailableBooksView, BookViewSet, PageDetailView, ReceiptViewSet
 
 urlpatterns = [
     path('books/', BookViewSet.as_view({'get': 'list'}), name='book-list'),
+    path('available-books/', AvailableBooksView.as_view(), name='available-books'),
     path('books/<int:pk>/', BookViewSet.as_view({'get': 'retrieve'}), name='book-detail'),
     path('books/<int:book_id>/pages/', PageDetailView.as_view({'get': 'retrieve'}), name='page-detail'),
     path('books/<int:book_id>/pages/<int:page_number>/', PageDetailView.as_view({'get': 'retrieve'}), name='page-number-detail'),
--- books/views.py
+++ books/views.py
@@ -1,19 +1,32 @@
 from rest_framework import viewsets, mixins
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.http import Http404
 
 from books.permissions import IsPurchased
-from .models import Book, Page, Receipt
+from .models import Book, Page, Receipt, UserBookAccess
 from .serializers import BookSerializer, PageDetailSerializer, ReceiptSerializer
 from .utils import send_receipt_to_moderator
 
 class BookViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class AvailableBooksView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        user = request.user
+        user_books = UserBookAccess.objects.filter(user=user).select_related('book')
+        books = [access.book for access in user_books]
+        
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
 
 class PageDetailView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Page.objects.all()
