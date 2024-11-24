fdda9df3d2ca0e0e77ac1239f1565fe6cea0cacf
SANJIKS <sanzarmaratov588@gmail.com>
2024-11-24 18:18:42 +0600

    fix

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import logging, sys, asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from decouple import config
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -15,16 +14,9 @@ bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher()
 
 
-class ReceiptCallback(CallbackData, prefix="receipt"):
-    action: str
-    receipt_id: str
-
-
-@dp.callback_query(ReceiptCallback.filter(action="confirm"))
-async def approve_receipt(call: CallbackQuery, callback_data: ReceiptCallback):
-    receipt_id = callback_data.receipt_id
-    logger.info(f"User {call.from_user.id} confirmed receipt {receipt_id}")
-    
+@dp.callback_query(F.data.startswith('confirm'))
+async def approve_receipt(call: CallbackQuery):
+    receipt_id = call.split('_')[1]
     await call.message.answer(
         f"Аххххххххххахахах сигма\n"
         f"Вот айди твоего чека: {receipt_id}\n"
@@ -32,27 +24,14 @@ async def approve_receipt(call: CallbackQuery, callback_data: ReceiptCallback):
     )
 
 
-@dp.callback_query(ReceiptCallback.filter(action="reject"))
-async def reject_receipt(call: CallbackQuery, callback_data: ReceiptCallback):
-    logger.info(f"User {call.from_user.id} rejected receipt {callback_data.receipt_id}")
-    
+@dp.callback_query(F.data.startswith('reject'))
+async def reject_receipt(call: CallbackQuery):
     await call.message.answer("🙄")
 
 
 @dp.message(commands=["start"])
 async def start_handler(message: types.Message):
-    logger.info(f"User {message.from_user.id} started the bot.")
-    
-    keyboard = InlineKeyboardBuilder()
-    keyboard.button(
-        text="Confirm Receipt", callback_data=ReceiptCallback(action="confirm", receipt_id="12345")
-    )
-    keyboard.button(
-        text="Reject Receipt", callback_data=ReceiptCallback(action="reject", receipt_id="12345")
-    )
-    keyboard.adjust(2)
-    await message.answer("Choose an action:", reply_markup=keyboard.as_markup())
-
+    await message.answer('Salam Aleykum')
 
 
 async def main():
