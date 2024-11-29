a94d79a3dd8185e8edf0007384819ed32ba5bde0
SANJIKS <sanzarmaratov588@gmail.com>
2024-11-29 16:18:24 +0600

    фикс

--- books/admin.py
+++ books/admin.py
@@ -22,12 +22,12 @@ class BookAdmin(admin.ModelAdmin):
     def process_pdf(self, book):
         pdf_path = book.content.path
         total_pages = len(convert_from_path(pdf_path, 1))
-
         chunk_size = 10
-
-        book_pages_dir = 'books/book_pages'
-        if not os.path.exists(os.path.join(default_storage.location, book_pages_dir)):
-            os.makedirs(os.path.join(default_storage.location, book_pages_dir))
+        book_pages_dir = 'book_pages'
+        book_pages_full_path = os.path.join(default_storage.location, book_pages_dir)
+        
+        if not os.path.exists(book_pages_full_path):
+            os.makedirs(book_pages_full_path)
 
         for i in range(0, total_pages, chunk_size):
             images = convert_from_path(pdf_path, first_page=i + 1, last_page=min(i + chunk_size, total_pages))
@@ -35,14 +35,17 @@ class BookAdmin(admin.ModelAdmin):
             for j, image in enumerate(images):
                 image_filename = f'{book.id}_page_{i + j + 1}.png'
                 image_path = os.path.join(book_pages_dir, image_filename)
-                image.save(image_path)
+                image_full_path = os.path.join(book_pages_full_path, image_filename)
+                
+                image.save(image_full_path)
 
-                page = Page.objects.create(
-                    book=book,
-                    number=i + j + 1,
-                    content=default_storage.save(image_path, ContentFile(open(image_path, 'rb').read()))
-                )
+                with open(image_full_path, 'rb') as img_file:
+                    page = Page.objects.create(
+                        book=book,
+                        number=i + j + 1,
+                        content=default_storage.save(image_path, ContentFile(img_file.read()))
+                    )
 
-                os.remove(image_path)
+                os.remove(image_full_path)
 
 admin.site.register([Page, Receipt, UserBookAccess, BonusBook])
