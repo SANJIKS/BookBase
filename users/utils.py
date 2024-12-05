a5ce38606a953ab226fb3e85b1e3bc6688891df3
SANJIKS <sanzarmaratov588@gmail.com>
2024-12-05 15:30:01 +0600

    infobip tria: l

--- users/utils.py
+++ users/utils.py
@@ -7,8 +7,13 @@ SMS_NIKITA_LOGIN = config("SMS_NIKITA_LOGIN")
 SMS_NIKITA_PASSWORD = config("SMS_NIKITA_PASSWORD")
 SMS_NIKITA_SENDER = config("SMS_NIKITA_SENDER")
 
+INFOBIP_URL = config('INFOBIP_URL')
+INFOBIP_API_KEY = config("INFOBIP_API_KEY")
 
-def send_sms(phone_number, pin):
+url = f"https://{INFOBIP_URL}/sms/2/text/advanced"
+
+
+def send_sms_nikita(phone_number, pin):
     text = f"Код подтверждения: {pin}\nЕсли вы не запрашивали код, просто проигнорируйте это сообщение."
     
     message_id = "MSG" + str(abs(hash(phone_number + pin)) % (10 ** 12))
@@ -31,4 +36,31 @@ def send_sms(phone_number, pin):
 
     response = requests.post(SMS_NIKITA_URL, headers=headers, data=xml_data)
 
+    return response.text
+
+
+def send_sms(phone_number, pin):
+    text = f"Код подтверждения: {pin}\nЕсли вы не запрашивали код, просто проигнорируйте это сообщение."
+    
+    payload = {
+        "messages": [
+            {   
+                "from": "Адалды Танда",
+                "destinations": [
+                    {
+                        "to": phone_number
+                    }
+                ],
+                "text": text
+            }
+        ]
+    }
+
+    headers = {
+        'Authorization': f'App {INFOBIP_API_KEY}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
     return response.text
